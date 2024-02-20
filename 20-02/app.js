90672a5c6ac53b5baf28f14ac6725061585509ab
yogi365 <sharma.yogendra@yahoo.com>
2024-02-20 14:36:37 +0530

    fifth commit

---
+++ 20-02/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const bodyParse = require('body-parser');
+const cors = require('cors');
+
+app.use(express.static('./public'))
+// app.use(bodyParse.json())
+app.use(bodyParse.urlencoded({extended:false}))
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.sendFile('./view/home.html',{root:'./'})
+});
+
+app.get('/url',(req,res)=>{
+    console.log(req.body);
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile('./view/about.html',{root:'./'})
+});
+
+app.get('/contact',(req,res)=>{
+    res.sendFile('./view/contact.html',{root:'./'})
+});
+
+app.post('/demo',(req,res)=>{
+    console.log(req.body); 
+    res.json("{'message':'Message recived'}")   
+})
+
+app.get('*',(req,res)=>{
+    res.send('<h1>Page not found</h1>')
+})
+
+app.listen(8080);
