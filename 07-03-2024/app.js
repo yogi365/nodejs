7f119b7cd4c6fad7197cbe691d3a02e011177419
yogi365 <sharma.yogendra@yahoo.com>
2024-03-11 14:21:52 +0530

    Gallery

--- 07-03-2024/app.js
+++ 07-03-2024/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require("fs");
+const cors = require('cors');
+app.use(cors())
 app.listen(3030)
 const multer = require('multer');
+app.use(express.static('./public'))
 const storage = multer.diskStorage({
-    destination:'./upload',
+    destination:'./public/images',
     filename:function(req,file,cb){
-        cb(null,file.filename+"-"+Date.now()+""+path.extname(file.originalname))
+        cb(null,Date.now()+""+path.extname(file.originalname))
     }
 }) 
 const upload = multer({storage:storage});
@@ -14,4 +18,12 @@ const upload = multer({storage:storage});
 app.post('/fileUpload',upload.array('images',10),(req,res)=>{
     console.log(req.files)
     res.send('file uploaded');
+})
+
+app.get('/gallery',(req,res)=>{
+    const files =  fs.readdirSync('./public/images');
+    const filePath = files.map((file)=>{
+        return '/images/'+file;        
+    })
+    res.json(filePath)
 })
